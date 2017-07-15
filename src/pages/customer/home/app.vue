3ef346f8af2d20407b8c2126ce48c6fa64514469
myMine-ming <862912335@qq.com>
2017-07-15 16:19:27 +0800

    first

---
+++ src/pages/customer/home/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="dd">
+      Home
+    
+    </div>
+    <img v-bind:src="logoImg">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+  import logo from 'assets/img/logo.png'
+  export default {
+    data () {
+      return {
+        msg: 'Use Vue 2.0 Today!',
+        logoImg: logo
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/pages/user/login/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="bg"></div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  import logo from 'assets/img/logo.png'
+
+  export default {
+    data(){
+      return {
+        msg: "测试数据"
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .bg
+    background url("img/logo.png") 100% 100%
+    background-size 100% 100%
+    width 300px
+    height 300px
+
+
+</style>
